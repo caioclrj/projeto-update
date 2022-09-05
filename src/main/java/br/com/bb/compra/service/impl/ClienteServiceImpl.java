be359e617bb3a9203abeae1ae537d42e79f2bc79
caioclrj <caio.c.lamarao@gmail.com>
2022-09-05 17:08:30 -0300

    Commit inicial

---
+++ src/main/java/br/com/bb/compra/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,53 @@
+package br.com.bb.compra.service.impl;
+
+import br.com.bb.compra.model.Cliente;
+import br.com.bb.compra.model.entity.ClienteEntity;
+import br.com.bb.compra.repository.ClienteRepository;
+import br.com.bb.compra.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static br.com.bb.compra.converter.ClienteConverter.convertDtoTo;
+import static br.com.bb.compra.converter.ClienteConverter.convertEntityTo;
+
+@ApplicationScoped
+@Named("clienteServiceImpl")
+@RequiredArgsConstructor
+public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Override
+    public void salvarCliente(Cliente cliente) {
+        clienteRepository.save(convertDtoTo(cliente));
+    }
+
+    @Override
+    public List<Cliente> getClientes() {
+        return convertEntityTo(clienteRepository.findAll());
+    }
+
+    @Override
+    public Cliente findByEmail(String email) {
+        final ClienteEntity entity = clienteRepository.findByEmail(email);
+        return convertEntityTo(entity);
+    }
+
+    // @Override
+    // public void atualizarCliente(@Valid Cliente cliente) {
+    //     
+    //     final ClienteEntity entity = ClienteEntity.builder()
+    //             .id(cliente.getId())
+    //             .email(cliente.getEmail())
+    //             .cpf(cliente.getCpf())
+    //             .nome(cliente.getNome())
+    //             .build();
+    //     clienteRepository.save(entity);
+    //     //cliente.setId(entity.getId());
+    // }
+}
